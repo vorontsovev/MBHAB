ffef90d91fdf3887f3d29d7d1219454957099cd1
vorontsovev <vorontsov.ev@gmail.com>
2017-04-05 01:49:37 +0600

    Пример структуры для передачи инициализационных параметров

--- mbhab.ino
+++ mbhab.ino
@@ -1,8 +1,18 @@
-struct SControllerSetup
+#include <Arduino.h>
+
+struct CI_CWATERRELAY {
+  uint8_t cw_port;
+  uint8_t hw_port;
+  uint8_t bw_port;
+  uint8_t bv_port;
+};
+
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(2400);
+  CI_CWATERRELAY init_water_relay = {.cw_port = 1, .hw_port = 2, .bw_port = 3, .bv_port = 4};
+  Serial.println(sizeof(init));
 }
 
 void loop() {
