206304af76099fff8b2183727ab5b81ddd28042d
vorontsovev <vorontsov.ev@gmail.com>
2017-04-05 17:01:07 +0500

    незначительные измнения

--- micro/micro.ino
+++ micro/micro.ino
@@ -4,13 +4,15 @@
 #include "CDisplay.h"
 #include "CModbusRegisters.h"
 
-CBMP180SensorInit         bmp180init = {.type=_CBMP180SENSOR, .period=5000};
-CWaterRelayInit           wrInit = {.type=_CWATERRELAY, .rs_address=0, .cv_port=11, .hv_port=10, .bv_port=9, .bp_port=8};
-CModbusRTUConnectorInit   mbInit = {.type=_CMODBUSRTUCONNECTOR, .rx=2, .tx=3, .rate=2400};
 
 CController *controller = new CController();
 
 void setup() {
+
+CBMP180SensorInit         bmp180init = {.type=_CBMP180SENSOR, .period=5000};
+CWaterRelayInit           wrInit = {.type=_CWATERRELAY, .rs_address=0, .cv_port=11, .hv_port=10, .bv_port=9, .bp_port=8};
+CModbusRTUConnectorInit   mbInit = {.type=_CMODBUSRTUCONNECTOR, .rx=2, .tx=3, .rate=2400};
+  
   #ifndef __NODEBUG__
     Serial.begin(115200);
     Serial.println(F("DEBUG MODE ON"));
